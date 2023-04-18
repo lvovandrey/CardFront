07ed0d65af1348427770856d5d18bd5086996904
a.lvov@retailrobotics.onmicrosoft.com <a.lvov@retailrobotics.onmicrosoft.com>
2023-04-18 22:05:53 +0300

    Добавил routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import './App.css';
 import React from 'react';
 import Header from './Header';
 import CardList from './CardList';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Game from './components/Game/Game';
+import Info from './components/Info/Info';
 
 function App() {
 
   return (
+    <div>
     <div className="App">
       <Header/>
       <CardList/>
+    </div>
+    <div color='#FF0000'>
+      <Routes>
+        <Route path="game" element={<Game />} />
+        <Route path="info" element={<Info />} />
+      </Routes>
+    </div>
+
     </div>
   );
 }
---
+++ src/components/Game/Game.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Game = () => {
+    return (
+        <div >
+            <h1>GAME</h1>
+        </div>
+    )
+}
+
+export default Game;
